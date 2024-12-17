cf0b5d87a7d2e3e27e02b64c5f823cec15d2bed4
vladUbuntu <volodkatraktorist@gmail.com>
2024-12-17 20:12:54 +0200

    init

---
+++ .hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/vlad/helomodule/hello.ko := ccache arm-eabi-ld -r -EL -z noexecstack -T ./scripts/module-common.lds -T ./arch/arm/kernel/module.lds  --build-id  -o /home/vlad/helomodule/hello.ko /home/vlad/helomodule/hello.o /home/vlad/helomodule/hello.mod.o ;  true
---
+++ .tmp_versions/hello.mod
@@ -0,0 +1,3 @@
+/home/vlad/helomodule/hello.ko
+/home/vlad/helomodule/hello.o
+
